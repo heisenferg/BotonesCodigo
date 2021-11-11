fbc8954b8df7a80fcceca39ee674d1360bccc9da
heisenferg <heisenferg@gmail.com>
2021-11-11 10:31:34 +0100

    2

--- app/src/main/java/com/example/botonescodigo/MainActivity.java
+++ app/src/main/java/com/example/botonescodigo/MainActivity.java
@@ -6,31 +6,54 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.GridLayout;
+import androidx.gridlayout.widget.GridLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        anadirHijos(18);
+        anadeHijos(18);
     }
 
-    public void anadirHijos(int c){
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLay);
+
+    public void recorrer() {
+        GridLayout g = findViewById(R.id.gridLay);
         Button b;
 
-        for (int i=0; i<c-1; i++){
+
+        for (int i = 0; i < g.getChildCount(); i++) {
+
+        }
+    }
+
+    public void anadeHijos(int c){
+        GridLayout g = findViewById(R.id.gridLay);
+        Button b;
+
+        for (int i=0; i<c; i++){
             b=new Button(this);
             b.setLayoutParams(new ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
 
             //Identificador único:
-           b.setId(View.generateViewId());
-            b.setText("Botón " + i);
-            gridLayout.addView(b, i);
+            b.setId(View.generateViewId());
+            if (i==17){
+                b.setText("reset");
+            } else {
+                String va = "Botón" + i;
+                b.setText(va);
+                g.addView(b, i);
+            }
         }
+        System.out.println("Filas: " + g.getRowCount());
+        System.out.println("Columnas: " + g.getColumnCount());
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
