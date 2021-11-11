4992f89b11b2df5a7e29239f0c76df7759082c91
heisenferg <heisenferg@gmail.com>
2021-11-11 09:13:30 +0100

    1

--- app/src/main/java/com/example/botonescodigo/MainActivity.java
+++ app/src/main/java/com/example/botonescodigo/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.botonescodigo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        anadirHijos(18);
+    }
+
+    public void anadirHijos(int c){
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLay);
+        Button b;
+
+        for (int i=0; i<c-1; i++){
+            b=new Button(this);
+            b.setLayoutParams(new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            //Identificador único:
+            b.setId(View.generateViewId());
+            b.setText("Botón " + i);
+            gridLayout.addView(b, i);
+        }
     }
 }
